b1d9392910d2485ff2c8455ce0ebdcb52a2fa16d
nohjuhyeon <njh2720@gmail.com>
2024-04-08 23:56:37 +0900

    push

--- docs/APIs/FHP_data.py
+++ docs/APIs/FHP_data.py
@@ -3,32 +3,46 @@
 import requests 
 import xmltodict
 # url 주소 변수 지정
+# sickCd = ['M542','S134']
+# medTp = 1,2
+import time
+from pymongo import MongoClient
+for i in range(2021,2023):
+    j= 2
+    url = 'https://apis.data.go.kr/B551182/diseaseInfoService/getDissByGenderAgeStats?'
+    params1 = {"serviceKey":"Qa6CXT4r6qEr%2BkQt%2FJx6wJr5MPx45hKNJwNTScoYryT2uGz7GozIqpjBw%2FRMk1uE8l92NU7h89m20sa%2FXHKuaQ%3D%3D",
+            "pageNo":"1",
+            "numOfRows":"18",
+            "year":i,
+            "sickCd":'S134',
+            "sickType":2,
+            "medTp":j
+            }
+    response = requests.get(url,params=params1)
+    data_dict = xmltodict.parse(response.text)
+    while True:
+        try:
+            if data_dict['response']['body']['items'] == None:
+                break
+            data_list = data_dict['response']['body']['items']['item']
+            for m in range(len(data_list)):
+                data_list[m]['year'] = i
+                data_list[m]['medTp']=j
+                # mongoDB 저장
+            # mongodb에 접속 -> 자원에 대한 class
+            mongoClient = MongoClient("mongodb://localhost:27017")
+            # database 연결
+            database = mongoClient["healthcare_sideproject"]
+            # collection 작업
+            collection = database['FHP_data']
+            # insert 작업 진행
+            result = collection.insert_many(data_list)
+            break
+        except:
+            time.sleep(3)
+            print('repeat')
 
-# for i in range(2012,2023):
-#     for j in range(1,3):
-#         for k in ['M542','S134']:
-#             url = 'https://apis.data.go.kr/B551182/diseaseInfoService/getDissByGenderAgeStats?'
-#             params1 = {"serviceKey":"ow0djIIbtYKcXjahX81pjlVfuA8kUj6DBQkALWCEeCXNuir3R0%2BLMOTTuhmW9Ms7R%2FAVfqb7cGIAazhHFttnPw%3D%3D",
-#                     "pageNo":"1",
-#                     "numOfRows":"10",
-#                     "year":i,
-#                     "sickCd":k,
-#                     "sickType":2,
-#                     "medTp":j
-#                     }
-# url = 'https://apis.data.go.kr/B551182/diseaseInfoService/getDissByGenderAgeStats?'
-# params1 = {"serviceKey":"ow0djIIbtYKcXjahX81pjlVfuA8kUj6DBQkALWCEeCXNuir3R0%2BLMOTTuhmW9Ms7R%2FAVfqb7cGIAazhHFttnPw%3D%3D",
-#         "pageNo":"1",
-#         "numOfRows":"1",
-#         "year":'2021',
-#         "sickCd":'M542',
-#         "sickType":'2',
-#         "medTp":'1'
-#         }
-url = 'https://apis.data.go.kr/B551182/diseaseInfoService/getDissByGenderAgeStats?serviceKey=Ix7Xj2a52fUYj6pPXF5TMooSl/dFBu/Y6/oyu8NyrmGGcfaMKwy99HEdnaPF5AAzDB0UBHEjkRjWrvJN7M5lPg==&numOfRows=10&pageNo=1&year=2022&sickCd=A00&sickType=1&medTp=1'
-response = requests.get(url)
-data_dict = xmltodict.parse(response.text)
-print(data_dict) 
+    pass
 
 
 
